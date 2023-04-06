aca6ef18c0fb13be1da67267c9c7afe5c2a42ce4
carolyate <carolyate13@gmail.com>
2023-04-06 00:39:47 -0500

    otraparte_siuu

---
+++ carol/menuu.py
@@ -0,0 +1,43 @@
+import sqlite3
+from funciones import *
+
+conn = sqlite3.connect('C:\\proyecto\\hotelproyecto\\carol\\cerberustest.db')
+
+while True:
+    print("Seleccione una opción:")
+    print("1 - Seleccionar ")
+    print("2 - Modificar ")
+    print("3 - Eliminar ")
+    print("4 - Agregar ")
+    print("5 - Salir")
+
+    opcion = int(input("ingrese la opcion: "))
+
+    match opcion:
+        case 1:
+            print("ingrese los suientes datos solicitados: ")
+            seleccion(input("ingrese tabla: "),input("ingrese campo: "),input("ingrese operador: "),input("ingrese dato: "))
+            print(seleccion)
+            
+        case 2:
+            print("ingrese los suientes datos solicitados: ")
+            modificar(input("ingrese tabla: "),input("ingrese campo: "),input("ingrese dato: "),input("ingrese id: "))
+            print(modificar)
+
+        case 3:
+            print("ingrese los suientes datos solicitados: ")
+            eliminar(input("ingrese tabla: "),input("ingrese campo: "),input("ingrese dato a borrar: "))
+            print(eliminar)
+
+        case 4:
+            print("ingrese los suientes datos solicitados: ")
+            insertar(input("ingrese tabla: "),input("ingrese id del servicio: "),input("cantidad: "),input("id tipo de servicio: "),input("id de factura: "))
+            print(insertar)
+
+        case 5:
+            print("Adiós!")
+            conn.close()
+            exit()
+
+        case _:
+            print("Opción inválida")
--- carol/pydb.py
+++ carol/pydb.py
@@ -1,12 +1,22 @@
 import sqlite3
-con=sqlite3.connect('C:\\cerberus\\Yate\\sqlite-tools\\db\\cerberus.db')
+con=sqlite3.connect('C:\\proyecto\\hotelproyecto\\carol\\cerberustest.db')
 #con=sqlite3.connect('pythonsqlite/pythondb.db')
 print(type(con))
 micursor=con.cursor()
 print(type(micursor))
 
 
-new_sql="SELECT * from usuario;"
+new_sql="SELECT * from tipo_servicio;"
+micursor.execute(new_sql)
+lista=micursor.fetchall()
+for fila in lista:
+    print(fila[0])
+    print(fila[1])
+    print(fila[2])
+    print('*'*50)
+    
+
+new_sql="SELECT * from servicio;"
 micursor.execute(new_sql)
 lista=micursor.fetchall()
 for fila in lista:
